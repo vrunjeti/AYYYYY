4d5130946c7d3ba8a0043d0fd788fb77376293e1
Faye413 <Faye@Fayes-MacBook-Pro.local>
2015-07-17 23:28:22 -0400

    added registration
    with html form and js

--- app/scripts/controllers/create.js
+++ app/scripts/controllers/create.js
@@ -1,17 +1,44 @@
 'use strict';
 
-/**
+*
  * @ngdoc function
  * @name bldrApp.controller:CreateCtrl
  * @description
  * # CreateCtrl
  * Controller of the bldrApp
- */
-angular.module('bldrApp')
+ 
+/*angular.module('bldrApp')
   .controller('CreateCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+  	var vm = this;
+
+  	vm.a = 'hi';
+
+  });
+*/
+
+angular.module('bldrApp')
+  .controller('CreateCtrl', function ($http, $window, $location) {
+    
+
+    var vm = this;
+	var url = 'http://localhost:8000/sql/';
+
+	vm.insert = function(formData){
+
+
+
+		var url = 'http://localhost:8000/auth/signup';
+		$http
+		.post(url , {
+			name : formData.name, 
+			location : formData.location,
+			description : formData.description,
+			participants : formData.participants
+			//image : formData.image
+			
+		})
+	
+		
+	};
   });
