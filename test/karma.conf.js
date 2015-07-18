3af6c718b0305f3b8bc1c53a691cb36102b8f014
Emily Lu <elu@wayfair.com>
2015-07-18 01:26:21 -0400

    reupdated bower

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -24,8 +24,8 @@ module.exports = function(config) {
       // bower:js
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
-      'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular-route/angular-route.js',
+      'bower_components/materialize/bin/materialize.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
