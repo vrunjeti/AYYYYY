d514749de729bdf5c10d5d4fc3b9e53eec1e3c04
Emily Lu <elu@wayfair.com>
2015-07-18 14:20:35 -0400

    project signup and photos added

--- app/scripts/controllers/project.js
+++ app/scripts/controllers/project.js
@@ -12,8 +12,30 @@ angular.module('bldrApp')
     $scope.id = $routeParams.id;
     var baseUrl = 'http://localhost:3000/api/';
 
+
 	  $http.get(baseUrl + 'projects/' + $scope.id)
 	    .success(function (data) {
 	      $scope.projectData = data;
 	    });
+
+
+    $(document).ready(function(){
+      var options = [
+        {selector: '.class', offset: 200, callback: 'globalFunction()' },
+        {selector: '.other-class', offset: 200, callback: 'globalFunction()'},
+        {selector: '#staggered-test', offset: 205, callback: 'Materialize.toast("Please continue scrolling!", 1500 )' },
+        {selector: '#staggered-test', offset: 400, callback: 'Materialize.showStaggeredList("#staggered-test")' },
+        {selector: '#image-test-1', offset: 0, callback: 'Materialize.fadeInImage("#image-test-1")' },
+        {selector: '#image-test-2', offset: 0, callback: 'Materialize.fadeInImage("#image-test-2")' },
+        {selector: '#image-test-3', offset: 0, callback: 'Materialize.fadeInImage("#image-test-3")' },
+        {selector: '#image-test-4', offset: 0, callback: 'Materialize.fadeInImage("#image-test-4")' },
+        {selector: '#image-test-5', offset: 0, callback: 'Materialize.fadeInImage("#image-test-5")' },
+        {selector: '#image-test-6', offset: 0, callback: 'Materialize.fadeInImage("#image-test-6")' },
+
+      ];
+      $('.modal-trigger').leanModal();
+      Materialize.scrollFire(options);
+    });
+
+
   });
